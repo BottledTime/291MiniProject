ddc28e5584eb7a1fc61ddd639d92667b1d794449
Derek Shultz <shultz.derek@gmail.com>
2015-10-28 15:06:53 -0600

    to merge with

--- agents.py
+++ agents.py
@@ -9,15 +9,15 @@ def recordDepart(email, CONN_STRING):
     # check if the user entered a value
     if len(flightno) == 0:
         print("Flight number cannot be blank")
-        return main.menu(email, CONN_STRING)
+        main.menu(email, CONN_STRING)
         
     # get more user input
     dep_date = input("Please enter the departure date(DD/MM/YYYY): ")
 
     if len(dep_date) == 0:
         print("Departure date must have a value.")
-        return main.menu(email, CONN_STRING)
-
+        main.menu(email, CONN_STRING)
+    
     # check if the user's input flight exists
     sql ="""
     select * from sch_flights 
@@ -28,11 +28,11 @@ def recordDepart(email, CONN_STRING):
         output = main.sqlWithReturn(sql, CONN_STRING)
     except:
         print("Improper input. Please try again")
-        return main.menu(email, CONN_STRING)
+        main.menu(email, CONN_STRING)
     if len(output) == 0:
         print("There is no flight with that flight number and departure date")
-        return main.menu(email, CONN_STRING)
-
+        main.menu(email, CONN_STRING)
+    
     # get the value of the date and time of the actual departure time
     act_dep_time = input("Please enter the actual departure time(DD/MM/YYYY HH24:MI): ")
     
@@ -45,12 +45,11 @@ def recordDepart(email, CONN_STRING):
     except:
         # if it does not, tell the user and return to the menu
         print("failed to record")
-        return main.menu(email, CONN_STRING)
-
+        main.menu(email, CONN_STRING)
     # if it does, tell the user and return to the menu
     print("successfully recorded")
-    return main.menu(email, CONN_STRING)
-
+    main.menu(email, CONN_STRING)
+    
 # This function is accessible to airline agents and takes their input to assign
 # an actual arrival date and time to flightnos in sch_flights
 def recordArr(email, CONN_STRING):
@@ -60,16 +59,16 @@ def recordArr(email, CONN_STRING):
     # check if the user actually entered anything
     if len(flightno) == 0:
         print("Flight number cannot be blank")
-        return main.menu(email, CONN_STRING)
-
+        main.menu(email, CONN_STRING)
+        
     # get the user's flight's desired departure date
     dep_date = input("Please enter the departure date(DD/MM/YYYY): ")
     
     # check if the user actually entered anything
     if len(dep_date) == 0:
         print("Departure date must have a value.")
-        return main.menu(email, CONN_STRING)
-
+        main.menu(email, CONN_STRING)
+           
     # check if the user's input flight exists
     sql ="""
     select * from sch_flights 
@@ -80,10 +79,11 @@ def recordArr(email, CONN_STRING):
         output = main.sqlWithReturn(sql, CONN_STRING)
     except:
         print("Improper input. Please try again")
-        return main.menu(email, CONN_STRING)
+        main.menu(email, CONN_STRING)
     if len(output) == 0:
         print("There is no flight with that flight number and departure date")
-        return main.menu(email, CONN_STRING)
+        main.menu(email, CONN_STRING)
+        
     # get the departure date and time from the user
     act_arr_time = input("Please enter the actual arrival time(DD/MM/YYYY HH24:MI): ")
     
@@ -96,7 +96,7 @@ def recordArr(email, CONN_STRING):
     except:
         # if it can't, tell the user and go to the menu
         print("failed to record")
-        return main.menu(email, CONN_STRING)
+        main.menu(email, CONN_STRING)
     # if it can, tell the user and go to the menu
     print("successfully recorded")
-    return main.menu(email, CONN_STRING)
+    main.menu(email, CONN_STRING)
